e8e9e72c80ababcac35e83f0036fcc76b916ec34
GABRIEL DE ALMEIDA MONTEIRO <CG3033643@car.ifsp>
2025-03-10 15:20:57 -0300

    Carroussel atualizado com texto sobreposto

--- src/components/carroussel/index.js
+++ src/components/carroussel/index.js
@@ -2,9 +2,15 @@ import style from "./car.module.css"
 
 export default function Carroussel() {
     return(
-        <div>
-            <img className={style.img} src="/car.jpeg"></img>
+        <div className={style.img}>
+            <h1 className={style.imgtxt}>JUST DO IT.</h1>
+
+            <h3 className={style.imgtxt2}>Follow Your Dreams</h3>
+
+            <h4 className={style.imgtxt3}>By nike.</h4>
         </div>
+
+        
     )
 
 }
